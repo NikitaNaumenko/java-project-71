3308870531dbd0a1e9edb7773ac8827acaf0aa16
Nikita Naumenko <naumenkoniki@gmail.com>
2024-11-28 16:40:31 +0500

    finish step 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,12 +2,22 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-// import picocli.CommandLine.Option;
-// import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff v.1.0", description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
+    @Parameters(description = "Path to first file.")
+    private String filepath1;
+
+    @Parameters(description = "Path to second file.")
+    private String filepath2;
+
+    @Option(names = { "-f", "--format" }, paramLabel = "stylish", description = "output format [default: stylish]")
+    private String format = "stylish";
+
+
     @Override
     public Integer call() throws Exception { // your business logic goes here...
         // byte[] fileContents = Files.readAllBytes(file.toPath());
